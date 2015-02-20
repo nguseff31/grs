6e9745de2c12827fbdf790188f801f40f1200c99
Nikita Gusev <nguseff31@gmail.com>
2015-02-20 17:25:18 +0500

    Рефакторинг тестов #1

--- UnitTestProject1/DirectedNetworkTest.cs
+++ UnitTestProject1/DirectedNetworkTest.cs
@@ -8,37 +8,72 @@ using GeomertyNetworkWorker;
 
 namespace UnitTestProject1
 {
+    /// <summary>
+    /// Интеграционные тесты для yEdNetwork
+    /// </summary>
     [TestClass]
     public class DirectedNetworkTest : ExampleTest
     {
+        private yEdDirectedNetwork network;
+
+        [TestInitialize]
+        public void InitNetwork()
+        {
+            network = new yEdDirectedNetwork();
+
+            network.setSymbols(this.symbols_to_classes);
+            network.loadFromXml(this.getExample("test"));
+        }
         protected override string getRelativePath()
         {
             return "";
         }
         [TestMethod]
+        [TestCategory("Integration")]
         public void checkDirectory()
         {
             Assert.IsTrue(Directory.Exists(getFixturePath()));
         }
 
         [TestMethod]
-        public void loadingExample()
+        [TestCategory("Integration")]
+        public void vertexesAndEdgesLoaded()
         {
-            yEdDirectedNetwork network = new yEdDirectedNetwork();
-            network.setSymbols(this.symbols_to_classes);
+            Assert.AreEqual<int>(9, network.Vertexes.Count());
+            Assert.AreEqual<int>(9, network.Edges.Count());
+        }
 
-            network.loadFromXml(this.getExample("test"));
-
-            int count = network.Vertexes.Count();
-
-            Assert.AreEqual<int>(9, count);
-
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void getAdjacentTest()
+        {
             Assert.IsTrue(compareCollections(new int[] { 2, 3, 4 }, network.getAdjacentEdges(3)));
 
             Assert.IsTrue(compareCollections(new int[] { 4, 5 }, network.getSuccessors(3)));
             Assert.IsTrue(compareCollections(new int[] { 2 }, network.getPredecessors(3)));
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void getFeatureTest()
+        {
+            Assert.AreEqual(network.getEdgeFeature(1).GetType(), typeof(Edge<CommonJunction>));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        [TestCategory("UnitTest")]
+        public void getFeatureFailTest()
+        {
+            var edge = network.getEdgeFeature(16);
+        }
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void containsTest()
+        {
             Assert.IsTrue(network.containsEdge(1, 2));
             Assert.IsFalse(network.containsEdge(2, 1));
+            Assert.IsTrue(network.containsVertex(1));
+            Assert.IsFalse(network.containsVertex(15));
         }
     }
 }
--- UnitTestProject1/NetworkAnalystTest.cs
+++ UnitTestProject1/NetworkAnalystTest.cs
@@ -9,17 +9,18 @@ namespace UnitTestProject1
     [TestClass]
     public class NetworkAnalystTest : ExampleTest
     {
+        private yEdDirectedNetwork network;
+
         protected override string getRelativePath()
         {
             return "";
         }
-        [TestMethod]
-        public void testArmatAlgInMemory()
-        {
-            yEdDirectedNetwork network = new yEdDirectedNetwork();
-            network.setSymbols(this.symbols_to_classes);
 
-            DirectedNetworkTracer tracer = new DirectedNetworkTracer(network);
+        [TestInitialize]
+        public void initInMemoryNetwork() 
+        {
+            network = new yEdDirectedNetwork();
+            network.setSymbols(this.symbols_to_classes);
             Dictionary<int, Vertex<CommonJunction>> vs = new Dictionary<int, Vertex<CommonJunction>>()
             {
                 { 1, new Vertex<CommonJunction>(1, new CommonJunction(1, DirectedNetworkTracer.GRS_CLASS_ID, ""))},
@@ -32,6 +33,34 @@ namespace UnitTestProject1
                 { 2, new Edge<CommonJunction>(2, 2, 3, new CommonJunction()) },
             };
             network.loadFromData(vs, es);
+        }
+
+        private bool testArmTask(IDirectedNetwork<CommonJunction,CommonJunction> network, IDirectedNetwork<CommonJunction,CommonJunction> expected_result, int edgeID) 
+        {
+            DirectedNetworkTracer tracer = new DirectedNetworkTracer(network);
+            IDirectedNetwork<CommonJunction, CommonJunction> result = tracer.armTask(edgeID).network;
+            return compareNetworks(result, expected_result);
+        }
+
+        private bool testArmExample(string name, int EdgeID) 
+        {
+            yEdDirectedNetwork network = new yEdDirectedNetwork();
+            network.setSymbols(this.symbols_to_classes);
+            network.loadFromXml(this.getExample(name));
+            DirectedNetworkTracer tracer = new DirectedNetworkTracer(network);
+
+            yEdDirectedNetwork expected_result = new yEdDirectedNetwork();
+            expected_result.setSymbols(this.symbols_to_classes);
+
+            expected_result.loadFromXml(this.getExample(name + "_result"));
+
+            IDirectedNetwork<CommonJunction, CommonJunction> result = tracer.armTask(EdgeID).network;
+            return compareNetworks(result, expected_result);
+        }
+        [TestMethod]
+        [TestCategory("Functional")]
+        public void testArmatAlgInMemory()
+        {
             Dictionary<int, Vertex<CommonJunction>> vs2 = new Dictionary<int, Vertex<CommonJunction>>(vs);
             Dictionary<int, Edge<CommonJunction>> es2 = new Dictionary<int, Edge<CommonJunction>>(es);
             vs2.Remove(1);
@@ -41,29 +70,24 @@ namespace UnitTestProject1
             result_network.setSymbols(this.symbols_to_classes);
 
             result_network.loadFromData(vs2, es2);
-            IDirectedNetwork<CommonJunction, CommonJunction> network2 = tracer.armTask(2).network;
-            Assert.IsTrue(compareNetworks(result_network, network2));
+            Assert.IsTrue(testArmTask(network, result_network, 2));
         }
-
+        private bool 
         [TestMethod]
+        [TestCategory("Functional")]
         public void testArmatAlg()
         {
-            yEdDirectedNetwork network = new yEdDirectedNetwork();
-            network.setSymbols(this.symbols_to_classes);
+            string[] examples = new string[] { "test_arm" };
+            int[] edges = new int[] { 12 };
 
-            DirectedNetworkTracer tracer = new DirectedNetworkTracer(network);
-
-            network.loadFromXml(this.getExample("test_arm"));
-
-            yEdDirectedNetwork result_network = new yEdDirectedNetwork();
-            result_network.setSymbols(this.symbols_to_classes);
-
-            result_network.loadFromXml(this.getExample("test_arm_result"));
-            IDirectedNetwork<CommonJunction, CommonJunction> network2 = tracer.armTask(12).network;
-            Assert.IsTrue(compareNetworks(result_network, network2));
+            for(int i = 0; i < examples.Length; i++) 
+            {
+                Assert.IsTrue(testArmExample(examples[i], edges[i]), "Ошибка в примере " + examples[i]);
+            }
         }
 
         [TestMethod]
+        [TestCategory("Functional")]
         [ExpectedException(typeof(ArgumentException))]
         public void testArmatAlgFail()
         {
